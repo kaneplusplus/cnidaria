ed0955a781c122c4bf2c30e4a8c57019dfa2bccc
Michael Kane <kaneplusplus@gmail.com>
2016-10-09 21:52:04 -0400

    Converted dvector to S4. Need to do this to dmatrix.

--- dvector.r
+++ dvector.r
@@ -2,6 +2,9 @@ library(foreach)
 
 source("convert_indices.r")
 
+
+setClass('dvector', representation(e='environment'))
+
 # TODO: Add code to make sure factors levels are normalized across
 # the passed vectors.
 dvector = function(l, part_constructor) {
@@ -12,39 +15,73 @@ dvector = function(l, part_constructor) {
 
   lens = sapply(l, length)
   len = sum(lens)
-  ret = list(parts=lapply(l, as_part, part_constructor), 
-             length=len, lengths=lens)
-  class(ret) = c("dvector", class(ret))
-  ret
+  e = new.env(parent=emptyenv())
+  assign("parts", lapply(l, as_part, part_constructor), envir=e)
+  assign("length", len, envir=e)
+  assign("lengths", lens, envir=e)
+  new('dvector', e=e)
+#  ret = list(parts=lapply(l, as_part, part_constructor), 
+#             length=len, lengths=lens)
+#  class(ret) = c("dvector", class(ret))
+#  ret
 }
 
-`[.dvector` = function(x, i) {
-  if (missing(i)) {
+setMethod("[",
+  signature(x='dvector', i="missing"),
+  function(x) {
     # TODO: we need a warning that the user probably doesn't want to 
     # emerge the entire distributed vector if the size is above a threshold.
-    unlist(sapply(x$parts, get_values))
-  } else if (is.numeric(i)) {
+    unlist(sapply(x@e$parts, get_values))
+  })
+
+setMethod("[",
+  signature(x='dvector', i="numeric"),
+  function(x, i) {
     ret = rep(NA, length(i))
-    ci = convert_indices(x$lengths, i)
+    ci = convert_indices(x@e$lengths, i)
     for (cn in unique(ci[,"part"])) {
-      ret[ci[,"part"] == cn] = get_values(x$parts[[cn]], 
+      ret[ci[,"part"] == cn] = get_values(x@e$parts[[cn]], 
         ci[ci[,"part"]==cn,"index"])
     }
     ret
-  } else {
-    stop("Index type not supported.")
-  }
-}
+  })
 
-length.dvector = function(x) {
-  x$length
-}
+#`[.dvector` = function(x, i) {
+#  if (missing(i)) {
+#    # TODO: we need a warning that the user probably doesn't want to 
+#    # emerge the entire distributed vector if the size is above a threshold.
+#    unlist(sapply(x$parts, get_values))
+#  } else if (is.numeric(i)) {
+#    ret = rep(NA, length(i))
+#    ci = convert_indices(x$lengths, i)
+#    for (cn in unique(ci[,"part"])) {
+#      ret[ci[,"part"] == cn] = get_values(x$parts[[cn]], 
+#        ci[ci[,"part"]==cn,"index"])
+#    }
+#    ret
+#  } else {
+#    stop("Index type not supported.")
+#  }
+#}
+
+setMethod("length", signature(x='dvector'), function(x) x@e$length)
+
+#length.dvector = function(x) {
+#  x$length
+#}
 
 # Arithmetic operators require an execution engine. We'll use foreach.
 
-Ops.dvector = function(e1, e2) {
-  FUN = get(.Generic, envir = parent.frame(), mode = "function")
-  if (class(e2) == "numeric" && length(e2) == 1)
-    dvector(foreach(part = e1$parts) %dopar% FUN(get_values(part), e2))
-}
+setMethod("Arith", signature(e1='dvector', e2='numeric'),
+  function(e1, e2) {
+    op = .Generic[[1]]
+    if (length(e2) == 1)
+    dvector(foreach(part = e1@e$parts) %dopar% op(get_values(part), e2))
+  })
+
+#Ops.dvector = function(e1, e2) {
+#  FUN = get(.Generic, envir = parent.frame(), mode = "function")
+#  if (class(e2) == "numeric" && length(e2) == 1)
+#    dvector(foreach(part = e1$parts) %dopar% FUN(get_values(part), e2))
+#}
 
