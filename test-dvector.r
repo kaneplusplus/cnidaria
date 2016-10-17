e85fcf0d4cfbba816f130e2b9c8aa92d62b7614d
Michael Kane <kaneplusplus@gmail.com>
2016-10-16 20:11:23 -0400

    testing with testthat.

--- test-dvector.r
+++ test-dvector.r
@@ -27,18 +27,17 @@ inds = sample.int(length(dv), 10, replace=TRUE)
 # Emerge the vector at the specified indices and see if it's
 # the same same as emerging the entire vector and then
 # subsetting by indices.
-any(dv[inds] != dv[][inds])
+expect_equal(dv[inds], dv[][inds])
 
 # Add one to each element of the vector.
 dv2 = dv + 1
 
 # Make sure it's the same as emerging dv and then adding 1.
-any(dv2[] != (dv+1)[])
+expect_equal(dv2[], (dv+1)[])
 
 dv_index = dvector_from_vectors(list(inds, inds))
 
-dv[dv_index][]
+expect_equal(dv[dv_index][], dv[][dv_index[]])
 
-dv_index = dvector_from_vectors(list(inds, inds))
 
 
